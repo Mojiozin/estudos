9b260281fa051a5f800d1a205a9c3918ec072bfb
Alan Davi <101908061+Mojiozin@users.noreply.github.com>
2025-01-22 18:24:24 +0000

    Código bonitinho fofo lindo que o Lorenzo arrumou fé estude sah bagaça

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,38 @@
+import { redirect } from "next/dist/server/api-utils";
+import { useRef, useState } from "react";
+
+const contents = [
+  {
+    label: "Discordar",
+    text: "Faça-me o Pix, só gradece 🙏",
+    color: "red",
+  },
+  {
+    label: "Concordar",
+    text: "Vai ter que fazer o PIX esquece 🙅‍♂️",
+    color: "green",
+  },
+  {
+    label: "Inclusivo",
+    text: "Só pra você enxergar João 👨‍🦯",
+    color: "yellow",
+  },
+];
+
 function Home() {
-    return <h1>Eu acho que o joão é maluco e dodoi, se concorda comigo manda um pix, se não me manda msg</h1>
+  const [state, setState] = useState({
+    text: "Eu acho que o João é maluco e dodói, se concorda comigo manda um pix se não manda tbm",
+    color: undefined,
+  });
+
+  return (
+    <>
+      <h1 style={{ color: state.color }}>{state.text}</h1>
+      {contents.map((content) => (
+        <button onClick={() => setState(content)}>{content.label}</button>
+      ))}
+    </>
+  );
 }
 
 export default Home;
